f01c948ebf0bf35d74548b86d32300f0f8094fd1
Jane Abernethy <jane.abernethy@hotmail.com>
2014-03-07 21:39:24 +1100

    remove nonlocal keyword for compatibility with python2.7.x

--- IndentX.py
+++ IndentX.py
@@ -26,44 +26,47 @@ def repeat_string(string, count):
     return output
 
 def indent_xml(xml, indent, initialDepth):
-    section = {'depth': 0, 
-                'prev':0, 
-                'add':False, 
-                'indent':'    ', 
-                'tag':re.compile(r'\s*(<[!/]?[^>]+>)\s*'),
-                'nl':'\n',
-                'comment': re.compile(r'^<!--'),
-                'close':re.compile(r'\s*<\/'),
-                'self_close':re.compile(r'\/>')}
+    class Section:
+        depth = 0
+        prev = 0
+        add = False
+        indent = '    '
+        tag = re.compile(r'\s*(<[!/]?[^>]+>)\s*')
+        nl = '\n'
+        comment = re.compile(r'^<!--')
+        close = re.compile(r'\s*<\/')
+        self_close = re.compile(r'\/>')
+
+    section = Section()
 
     def replace_with_indent(match):
         output = ''
         p = match.group(0)
 
-        if section['close'].search(p):
-            if section['depth'] == section['prev'] and section['add']:
+        if section.close.search(p):
+            if section.depth == section.prev and section.add:
                 output += p
-                section['depth'] -= 1
+                section.depth -= 1
             else:
-                section['depth'] -= 1
-                output += section['nl'] + repeat_string(section['indent'], section['depth']) + p
-            section['add'] = False
-        elif section['self_close'].search(p):
-            output += section['nl'] + repeat_string(section['indent'], section['depth']) + p
-            section['add'] = False
-        elif section['comment'].search(p):
-            output += section['nl'] + repeat_string(section['indent'], section['depth']) + p
+                section.depth -= 1
+                output += section.nl + repeat_string(section.indent, section.depth) + p
+            section.add = False
+        elif section.self_close.search(p):
+            output += section.nl + repeat_string(section.indent, section.depth) + p
+            section.add = False
+        elif section.comment.search(p):
+            output += section.nl + repeat_string(section.indent, section.depth) + p
         else:
-            output += section['nl'] + repeat_string(section['indent'], section['depth']) + p
-            section['depth'] += 1
-            section['add'] = True
+            output += section.nl + repeat_string(section.indent, section.depth) + p
+            section.depth += 1
+            section.add = True
 
-        section['prev'] = section['depth']
+        section.prev = section.depth
         return output
 
-    if section['depth'] != 0:
+    if section.depth != 0:
         print('Unmatched tags exist')
 
-    indented = section['tag'].sub(replace_with_indent, xml)
+    indented = section.tag.sub(replace_with_indent, xml)
     blank = re.compile(r'^\s*$\r?\n', re.M)
     return blank.sub('', indented)
